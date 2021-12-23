f0bfb259828f1b0b53f3eb5eaef79c1fa8d73632
stefanpascu <41235115+stefanpascu@users.noreply.github.com>
2021-12-23 22:57:44 +0200

    new update
    Also added sound when player moves on the led matrix

--- Proiect_Bomberman/Proiect_Bomberman.ino
+++ Proiect_Bomberman/Proiect_Bomberman.ino
@@ -176,6 +176,7 @@ void loop() {
     // matrix display logic
       updateMatrix();
       matrixChanged = false;
+      noNewTone(buzzerPin);
     }
         
   }
@@ -1213,6 +1214,7 @@ void updatePositions() {
   }
   // tests if the player was on a lighted led, if so, it lets it ligthed, otherwise it turns the led off
   if (xPos != xLastPos || yPos != yLastPos) {
+    NewTone (buzzerPin, NOTE_E4);
     matrixChanged = true;
     if (onLightedLed == false) {
       matrix[xLastPos][yLastPos] = 0;
